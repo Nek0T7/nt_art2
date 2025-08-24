9f2f98980df945251c14c65bcb1e1898baf0e738
Nek0T7 <naoya.tajima.econ@gmail.com>
2025-08-24 23:48:49 +0900

    4

--- prepareR.R
+++ prepareR.R
@@ -6,6 +6,7 @@ library(pacman)
 pacman::p_load(haven, tidyverse)
 library(haven)
 library(tidyverse)
+library(conflicted)
 
 #データを読み込み、CSVファイルとして保存する
 data_raw <-read_dta("artist_reputation_main.dta")
@@ -13,5 +14,8 @@ write.csv(data_raw,"artist_reputation_main.csv",row.names = FALSE)
 
 #データの確認
 data_raw
-head(data_raw)
 class(data_raw)
+
+#データの保存
+saveRDS(data_raw, file = "artist_reputation_main.rds")
+
